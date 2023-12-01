e9f3c55aa85235c12a8651eed02322dbefd3b4f4
Anupam Maurya <anupammaurya6767@gmail.com>
2023-12-01 10:51:35 +0530

    Update file_utils.py

--- modules/file_utils.py
+++ modules/file_utils.py
@@ -1,19 +1,19 @@
 # file_utils.py
 
-def read_credentials(filename='new_credentials.txt'):
+def read_credentials(filename='credentials.txt'):
     with open(filename) as file:
         username = file.readline().strip()
         password = file.readline().strip()
     return username, password
 
-def read_subjects(filename='new_subjects.txt'):
+def read_subjects(filename='subjects.txt'):
     subject_list = []
     with open(filename) as file:
         for subject in file.readlines():
             subject_list.append(subject.strip())
     return subject_list
 
-def read_parameters(filename='new_parameters.txt'):
+def read_parameters(filename='parameters.txt'):
     with open(filename) as file:
         website_loading_loop = int(file.readline().strip())
         subject_loading_loop = int(file.readline().strip())
